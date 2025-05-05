70dec21c3b1049cd30092ae33327daa0af8edf64
Darik Harter <darik.harter@gmail.com>
2025-05-04 21:10:32 -0600

    Start prompt

--- chai/commands/chat.py
+++ chai/commands/chat.py
@@ -1,3 +1,18 @@
+from prompt_toolkit import prompt
+
+
 def chat():
     """Chat with a Gemini model."""
-    print("chat")
+    while True:
+        try:
+            user_input = prompt(
+                "[model] >>> ",
+                multiline=True,
+                prompt_continuation="... ",
+                wrap_lines=False,
+            )
+            print(user_input)
+        except KeyboardInterrupt:
+            continue
+        except EOFError:
+            break
